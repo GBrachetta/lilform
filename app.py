f6e90c6a0f4ac3979b5585b7e865c07820ead8fd
Guillermo Brachetta <brachetta@me.com>
2020-04-26 02:59:19 +0200

    Added basic routes - Added basic styles+Bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,15 +8,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 
+
 # Script to create a table
+
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    added_builder = db.relationship('Builder', backref='author', lazy=True)
+    added_builder = db.relationship(
+        'Builder', backref='contributor', lazy=True)
     added_instrument = db.relationship(
-        'Instrument', backref='author', lazy=True)
+        'Instrument', backref='contributor', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username})"
@@ -27,7 +30,7 @@ class Builder(db.Model):
     name = db.Column(db.String(60), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     instruments_built = db.relationship(
-        'Instrument', backref='built', lazy=True)
+        'Instrument', backref='instrument_builder', lazy=True)
 
     def __repr__(self):
         return f"Builder('{self.name}')"
@@ -42,17 +45,46 @@ class Instrument(db.Model):
 
     def __repr__(self):
         return f"Instrument('{self.location}')"
+    
+
+instruments = [
+    {
+        'contributor': 'Guillermo Brachetta',
+        'instrument_maker': 'Vaudry',
+        'location': 'Amsterdam'
+    },
+    {
+        'contributor': 'Menno van Delft',
+        'instrument_maker': 'Ruckers',
+        'location': 'Paris'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home</h1>"
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html')
+
+
+@app.route('/builders')
+def builders():
+    return render_template('builders.html')
+
+
+@app.route('/instruments')
+def instrument():
+    return render_template('instruments.html', instruments=instruments, title='Instruments')
+
+
+@app.route('/contributors')
+def contributors():
+    return render_template('contributors.html')
 
 
 if __name__ == "__main__":
