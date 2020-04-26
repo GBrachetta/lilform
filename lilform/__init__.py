84e7906639dbe6ff808573df26a8cd33d1387dd5
Guillermo Brachetta <brachetta@me.com>
2020-04-26 19:42:07 +0200

    Login authentication

--- lilform/__init__.py
+++ lilform/__init__.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-mode = 'prod'
+mode = 'dev'
 
 if mode == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -16,5 +17,8 @@ else:
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from lilform import routes
--- lilform/forms.py
+++ lilform/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from lilform.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
@@ -12,10 +12,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
-
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('The username already exists.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email already exists.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[
                              DataRequired(), Length(min=6, max=12)])
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
